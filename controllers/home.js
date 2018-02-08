96997f6ce6ab460bb2344dbcf37f27f937ba9502
vecchiotom <vecchiotom1@gmail.com>
2018-02-08 19:38:27 +0100

    fixed some thingys ehehehehe

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,7 @@
 exports.index = function(request, response)
 {
     response.pageInfo = {}
-    response.pageInfo.name = "Synn.nl"
+    response.pageInfo.name = "IceHax.it"
     
     response.render('home/index', response.pageInfo);
 };
@@ -9,7 +9,7 @@ exports.index = function(request, response)
 exports.other = function(request, response)
 {
     response.pageInfo = {}
-    response.pageInfo.content = "This is the website of Synn.nl"
+    response.pageInfo.content = "This is the website of IceHax.it"
 
     response.render('home/other', response.pageInfo);
 };
